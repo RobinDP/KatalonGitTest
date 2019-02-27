47f18254fea86f9a7f672f92af91658a84351238
Robin de Pauw <robin.depauw@cegeka.com>
2019-02-27 14:49:24 +0100

    test message

---
+++ Test Suites/TestSuite1.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>08c83e0a-9fee-4a70-9d4c-67d071d0a793</testSuiteGuid>
+</TestSuiteEntity>
